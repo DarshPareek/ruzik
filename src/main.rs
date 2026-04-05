40fd447e27cd038e19e9f718be4982107fc20508
DarshPareek <pareekdarsh0162@gmail.com>
2026-04-05 23:08:48 +0530

    Screensaver Status achieved

--- src/main.rs
+++ src/main.rs
@@ -1,111 +1,93 @@
-use raylib::core::audio::RaylibAudio;
+use raylib::ffi;
 use raylib::prelude::*;
 use std::env;
-use std::i16::{MAX, MIN};
 use std::process::exit;
+
 static mut GLOBAL_FRAMES: [f32; 1024] = [0.0; 1024];
 static mut GLOBAL_FRAMES_LEN: usize = 0;
-fn callback(buffer_data: &mut [f32], frame: u32) {
-    // println!("Inside");
-    unsafe {
-        for i in 0..buffer_data.len() {
-            GLOBAL_FRAMES[i] = buffer_data[i];
-        }
-        GLOBAL_FRAMES_LEN = buffer_data.len();
+
+unsafe extern "C" fn audio_callback(buffer_data: *mut std::ffi::c_void, frames: u32) {
+    let f32_ptr = buffer_data as *const f32;
+    let channels = 2;
+    let total_samples = (frames * channels) as usize;
+    let limit = total_samples.min(1024);
+    for i in 0..limit {
+        GLOBAL_FRAMES[i] = *f32_ptr.add(i);
     }
-    // println!("Frame Data: {frame}");
+    GLOBAL_FRAMES_LEN = limit;
 }
+
 fn main() {
-    let (mut rl, thread) = raylib::init().size(1600, 450).title("Ruzik").build();
-    let audio_init = RaylibAudio::init_audio_device();
-    let audio: RaylibAudio;
-    match audio_init {
-        Ok(value) => {
-            println!("Audio Loading Success");
-            audio = value;
-        }
-        Err(e) => {
-            println!("Audio Loading Failed with {e}");
-            exit(1);
-        }
-    }
-    let new_music = audio.new_music("assets/Let It Happen.flac");
-    let music: Music;
-    match new_music {
-        Ok(value) => {
-            println!("music Loaded");
-            music = value;
-        }
-        Err(e) => {
+    let (mut rl, thread) = raylib::init()
+        .vsync()
+        .resizable()
+        .size(1980, 1080)
+        .title("Ruzik")
+        .fullscreen()
+        .build();
+    let audio = RaylibAudio::init_audio_device().unwrap_or_else(|e| {
+        println!("Audio Loading Failed with {e}");
+        exit(1);
+    });
+
+    let mut music = audio
+        .new_music("assets/One More Hour.flac")
+        .unwrap_or_else(|e| {
             let path = env::current_dir().unwrap();
             println!("Couldn't find music file {e} in path {}", path.display());
             exit(1);
-        }
-    }
+        });
+
     let mut pause = false;
     music.play_stream();
-    // attach_audio_stream_processor_to_music(&music, &mut callback);
-    let music_stream = music.stream;
-    let processor = &mut callback;
-    let audioprocessor = attach_audio_stream_processor_to_music(&music, processor);
+
+    unsafe {
+        ffi::AttachAudioStreamProcessor(music.stream, Some(audio_callback));
+    }
+
     let sw = rl.get_render_width();
-    let sh = rl.get_render_height() as i16;
+    let sh = rl.get_render_height() as i32;
+
     println!("FrameCount: {}", music.frameCount);
     println!("Samples per second: {}", music.stream.sampleRate);
-    println!("Samples per second: {}", music.stream.sampleSize);
-    println!("Samples per second: {}", music.stream.channels);
+    println!("Channels: {}", music.stream.channels);
+
     while !rl.window_should_close() {
         music.update_stream();
-        let played = music.get_time_played();
-        // println!("Already played: {played}");
+
         if rl.is_key_pressed(KeyboardKey::KEY_SPACE) {
             pause = !pause;
         }
+
         if pause {
             music.pause_stream();
         } else {
             music.resume_stream();
         }
-        let mut d = rl.begin_drawing(&thread);
 
-        d.clear_background(Color::DARKBLUE);
-        let mut cell_width: i32 = 0;
+        let mut d = rl.begin_drawing(&thread);
+        d.clear_background(Color::BLACK);
+
         unsafe {
             if GLOBAL_FRAMES_LEN > 0 {
-                cell_width = sw / (GLOBAL_FRAMES_LEN as i32);
-            }
-            for i in 0..GLOBAL_FRAMES_LEN {
-                let sample = (GLOBAL_FRAMES[i] * 1000.0) as i16;
-                println!("Sample {sample} i {i}");
-                if sample > 0 {
-                    let t = (sample / MAX);
-                    println!(
-                        "{}, {}, {}, {}, {}",
-                        (i as i32) * cell_width,
-                        (sh / 2 - sh / 2 * t) as i32,
-                        cell_width,
-                        (sh / 2) as i32,
-                        sample as f32 / 10.0
-                    );
-                    // d.draw_rectangle(x, y, width, height, color);
-                    d.draw_rectangle(
-                        (i as i32) * cell_width,
-                        (sh / 2 - sh / 2 * t) as i32,
-                        cell_width,
-                        ((sh / 2) as f32 * (sample as f32) / 30.0) as i32,
-                        Color::RED,
-                    );
-                } else {
-                    let t = sample / MIN;
-                    d.draw_rectangle(
-                        (i as i32) * cell_width,
-                        0,
-                        cell_width,
-                        ((sh / 2) as f32 * (sample as f32)) as i32,
-                        Color::RED,
-                    );
+                let step = (sw as f32) / (GLOBAL_FRAMES_LEN as f32);
+                for i in 0..GLOBAL_FRAMES_LEN {
+                    let sample = GLOBAL_FRAMES[i];
+                    let bar_height = (sample.abs() * (sh as f32)) as i32;
+                    let width = step.ceil() as i32;
+                    let x = (i as f32 * step) as i32;
+                    let y = sh / 2 - bar_height / 2;
+                    let color = if sample > 0.0 {
+                        Color::LIGHTGREEN
+                    } else {
+                        Color::MAROON
+                    };
+                    d.draw_rectangle(x, y, width, bar_height, color);
                 }
             }
         }
+    }
+    unsafe {
+        ffi::DetachAudioStreamProcessor(music.stream, Some(audio_callback));
     }
 }
