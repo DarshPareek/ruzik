ae1fcb0ae9ee7f67ff631befb22bffb025ce75bb
DarshPareek <pareekdarsh0162@gmail.com>
2026-04-09 02:38:45 +0530

    Visualisation Achieved

---
+++ src/fft.rs
@@ -0,0 +1,107 @@
+use num_complex::Complex;
+use std::f32::consts::PI;
+use std::sync::MutexGuard;
+const I: Complex<f32> = Complex { re: 0.0, im: 1.0 };
+
+pub fn fft(input: &Vec<Complex<f32>>) -> Vec<Complex<f32>> {
+    fn fft_inner(buf_a: &mut [Complex<f32>], buf_b: &mut [Complex<f32>], n: usize, step: usize) {
+        if step >= n {
+            return;
+        }
+        fft_inner(buf_b, buf_a, n, step * 2);
+        fft_inner(&mut buf_b[step..], &mut buf_a[step..], n, step * 2);
+        let (left, right) = buf_a.split_at_mut(n / 2);
+        for i in (0..n).step_by(step * 2) {
+            let t = (-I * PI * (i as f32) / (n as f32)).exp() * buf_b[i + step];
+            left[i / 2] = buf_b[i] + t;
+            right[i / 2] = buf_b[i] - t;
+        }
+    }
+    let n_orig = input.len();
+    let n = n_orig.next_power_of_two();
+    let mut buf_a = input.to_vec();
+    buf_a.append(&mut vec![Complex { re: 0.0, im: 0.0 }; n - n_orig]);
+    let mut buf_b = buf_a.clone();
+    fft_inner(&mut buf_a, &mut buf_b, n, 1);
+    buf_a
+}
+pub fn f32_to_complex(input: &Vec<f32>) -> Vec<Complex<f32>> {
+    let mut out_complex: Vec<Complex<f32>> = vec![];
+    for i in 0..input.len() {
+        out_complex.push(Complex {
+            re: input[i],
+            im: 0.0,
+        });
+    }
+    return out_complex;
+}
+
+pub fn f32_to_complex_mutex(input: &MutexGuard<'_, Vec<f32>>) -> Vec<Complex<f32>> {
+    let mut out_complex: Vec<Complex<f32>> = vec![];
+    for i in 0..input.len() {
+        out_complex.push(Complex {
+            re: input[i],
+            im: 0.0,
+        });
+    }
+    return out_complex;
+}
+
+pub fn amp(x: Complex<f32>) -> f32 {
+    if x.re.abs() < x.im.abs() {
+        return x.im.abs();
+    }
+    return x.re.abs();
+}
+
+// fn main() {
+//     let n: usize = 16;
+//     let mut inputs: Vec<Complex<f32>> = vec![];
+//     let mut out_complex: Vec<Complex<f32>> = vec![];
+//     for i in 0..n {
+//         let t = (i as f32) / (n as f32);
+//         let sample = (2.0 * PI * t).sin() + (2.0 * PI * t * 2.0).sin() + (2.0 * PI * t * 4.0).cos();
+//         inputs.push(Complex {
+//             re: sample,
+//             im: 0.0,
+//         });
+//         out_complex.push(Complex { re: 0.0, im: 0.0 });
+//     }
+
+//     // for f in 0..n / 2 {
+//     //     // out_complex[f] = Complex { re: 0.0, im: 0.0 };
+//     //     // out_complex[f + n / 2] = Complex { re: 0.0, im: 0.0 };
+//     //     //even
+//     //     for i in (0..n).step_by(2) {
+//     //         let t = (i as f32) / (n as f32);
+//     //         let v = inputs[i] * (Complex::i() * 2.0 * PI * t * (f as f32)).exp();
+//     //         out_complex[f] += v;
+//     //         out_complex[f + n / 2] += v;
+//     //         // println!("{i} {:?} {:?}", v.re, v.im);
+//     //     }
+//     //     for i in (1..n).step_by(2) {
+//     //         let t = (i as f32) / (n as f32);
+//     //         let v = inputs[i] * (Complex::i() * 2.0 * PI * t * (f as f32)).exp();
+//     //         out_complex[f] += v;
+//     //         out_complex[f + n / 2] -= v;
+//     //     }
+//     // }
+
+//     // for f in 0..n {
+//     //     out_sin.push(0.0);
+//     //     out_cos.push(0.0);
+//     //     out_complex.push(Complex { re: 0.0, im: 0.0 });
+//     //     for i in 0..n {
+//     //         let t = (i as f32) / (n as f32);
+//     //         out_complex[f] += inputs[i] * (Complex::i() * 2.0 * PI * t * (f as f32)).exp();
+//     //         out_sin[f] += inputs[i] * (2.0 * PI * t * (f as f32)).sin();
+//     //         out_cos[f] += inputs[i] * (2.0 * PI * t * (f as f32)).cos();
+//     //     }
+//     // }
+//     // println!("{:?}", out_sin);
+//     // println!("{:?}", out_cos);
+//     out_complex = fft(&inputs);
+//     for i in 0..out_complex.len() {
+//         println!("{:?} {:?}", out_complex[i].re, out_complex[i].im);
+//     }
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,35 @@
-use raylib::core::audio::RaylibAudio;
 use raylib::prelude::*;
-use std::env;
-use std::i16::{MAX, MIN};
-use std::process::exit;
-static mut GLOBAL_FRAMES: [f32; 1024] = [0.0; 1024];
-static mut GLOBAL_FRAMES_LEN: usize = 0;
-fn callback(buffer_data: &mut [f32], frame: u32) {
-    // println!("Inside");
-    unsafe {
-        for i in 0..buffer_data.len() {
-            GLOBAL_FRAMES[i] = buffer_data[i];
-        }
-        GLOBAL_FRAMES_LEN = buffer_data.len();
-    }
-    // println!("Frame Data: {frame}");
-}
+use std::{
+    ops::Range,
+    sync::{Arc, Mutex},
+};
+mod fft;
+use fft::*;
 fn main() {
-    let (mut rl, thread) = raylib::init().size(1600, 450).title("Ruzik").build();
-    let audio_init = RaylibAudio::init_audio_device();
-    let audio: RaylibAudio;
-    match audio_init {
-        Ok(value) => {
-            println!("Audio Loading Success");
-            audio = value;
+    let (mut rl, thread) = raylib::init()
+        .size(1280, 720)
+        .title("Ruzik - High Level API")
+        .build();
+
+    let audio = RaylibAudio::init_audio_device().expect("Audio failed");
+    let music = audio
+        .new_music("assets/DoIWannaKnow.wav")
+        .expect("File not found");
+
+    let shared_samples = Arc::new(Mutex::new(vec![]));
+    let samples_for_callback = Arc::clone(&shared_samples);
+    let mut my_processor = move |buffer: &mut [f32], _frames: u32| {
+        if let Ok(mut data) = samples_for_callback.lock() {
+            data.clear();
+            let limit = buffer.len().min(4096);
+            data.extend_from_slice(&buffer[..limit]);
         }
-        Err(e) => {
-            println!("Audio Loading Failed with {e}");
-            exit(1);
-        }
-    }
-    let new_music = audio.new_music("assets/Let It Happen.flac");
-    let music: Music;
-    match new_music {
-        Ok(value) => {
-            println!("music Loaded");
-            music = value;
-        }
-        Err(e) => {
-            let path = env::current_dir().unwrap();
-            println!("Couldn't find music file {e} in path {}", path.display());
-            exit(1);
-        }
-    }
-    let mut pause = false;
+    };
+    let _processor_handle = attach_audio_stream_processor_to_music(&music, &mut my_processor);
     music.play_stream();
-    // attach_audio_stream_processor_to_music(&music, &mut callback);
-    let music_stream = music.stream;
-    let processor = &mut callback;
-    let audioprocessor = attach_audio_stream_processor_to_music(&music, processor);
-    let sw = rl.get_render_width();
-    let sh = rl.get_render_height() as i16;
-    println!("FrameCount: {}", music.frameCount);
-    println!("Samples per second: {}", music.stream.sampleRate);
-    println!("Samples per second: {}", music.stream.sampleSize);
-    println!("Samples per second: {}", music.stream.channels);
+    let mut pause: bool = false;
     while !rl.window_should_close() {
         music.update_stream();
-        let played = music.get_time_played();
-        // println!("Already played: {played}");
         if rl.is_key_pressed(KeyboardKey::KEY_SPACE) {
             pause = !pause;
         }
@@ -67,44 +39,46 @@ fn main() {
             music.resume_stream();
         }
         let mut d = rl.begin_drawing(&thread);
-
-        d.clear_background(Color::DARKBLUE);
-        let mut cell_width: i32 = 0;
-        unsafe {
-            if GLOBAL_FRAMES_LEN > 0 {
-                cell_width = sw / (GLOBAL_FRAMES_LEN as i32);
-            }
-            for i in 0..GLOBAL_FRAMES_LEN {
-                let sample = (GLOBAL_FRAMES[i] * 1000.0) as i16;
-                println!("Sample {sample} i {i}");
-                if sample > 0 {
-                    let t = (sample / MAX);
-                    println!(
-                        "{}, {}, {}, {}, {}",
-                        (i as i32) * cell_width,
-                        (sh / 2 - sh / 2 * t) as i32,
-                        cell_width,
-                        (sh / 2) as i32,
-                        sample as f32 / 10.0
-                    );
-                    // d.draw_rectangle(x, y, width, height, color);
-                    d.draw_rectangle(
-                        (i as i32) * cell_width,
-                        (sh / 2 - sh / 2 * t) as i32,
-                        cell_width,
-                        ((sh / 2) as f32 * (sample as f32) / 30.0) as i32,
-                        Color::RED,
-                    );
-                } else {
-                    let t = sample / MIN;
-                    d.draw_rectangle(
-                        (i as i32) * cell_width,
-                        0,
-                        cell_width,
-                        ((sh / 2) as f32 * (sample as f32)) as i32,
-                        Color::RED,
-                    );
+        d.clear_background(Color::BLACK);
+        if let Ok(samples) = shared_samples.lock() {
+            let mut com_samples = f32_to_complex_mutex(&samples);
+            com_samples = fft(&com_samples);
+            let mut max_amp = 0.0;
+            for i in 0..com_samples.len() {
+                if max_amp < com_samples[i].re.abs() {
+                    max_amp = com_samples[i].re.abs();
                 }
+                if max_amp < com_samples[i].im.abs() {
+                    max_amp = com_samples[i].im.abs();
+                }
+            }
+            let sw = d.get_render_width() as f32;
+            let sh = d.get_render_height() as f32;
+            let step = sw / samples.len() as f32;
+            for i in 0..com_samples.len() {
+                let t = amp(com_samples[i]) / max_amp;
+                let x = (i as f32 * step) as i32;
+                let bar_h = (t * sh) as i32;
+                let color: Color = Color::RED;
+                d.draw_rectangle(
+                    x,
+                    (sh as i32) - (bar_h / 2),
+                    step.ceil() as i32,
+                    bar_h / 2 as i32,
+                    color,
+                );
+                // // if sample < 0.0 {
+                // //     color = Color::DARKRED;
+                // // } else {
+                // //     color = Color::LIGHTBLUE;
+                // // }
+                // d.draw_rectangle(
+                //     x,
+                //     (sh as i32 / 2) - (bar_h / 2),
+                //     step.ceil() as i32,
+                //     bar_h / 2 as i32,
+                //     color,
+                // );
             }
         }
     }
