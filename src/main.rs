982944d322c44ad73d113ab4b80e7d607e4c0c51
DarshPareek <pareekdarsh0162@gmail.com>
2026-04-16 23:42:57 +0530

    macroquad testing: audio loading too long

--- src/main.rs
+++ src/main.rs
@@ -1,94 +1,43 @@
-use raylib::prelude::*;
+use macroquad::{
+    audio::{self, PlaySoundParams, play_sound},
+    prelude::*,
+};
 
-mod fft;
-mod music;
-use music::*;
-
-const SAMPLE_SIZE: usize = 1 << 12;
-
-fn main() {
-    let (mut rl, thread) = raylib::init().size(1280, 720).title("Ruzik").build();
-    let audio = RaylibAudio::init_audio_device().expect("Audio failed");
-
-    // 1. Make music mutable so we can reassign it later
-    let mut music = load_music(
-        &audio,
-        "/home/darsh/devel/ruzik/assets/Let It Happen.flac".to_string(),
-    );
-    let sample_handler = init_sample_handler(SAMPLE_SIZE);
-    let mut processor = processor(sample_handler.clone_samples());
-    let mut processor_handle = Some(attach_audio_stream_processor_to_music(
+#[macroquad::main("BasicShapes")]
+async fn main() {
+    let music = audio::load_sound("/home/darsh/Music/output.wav")
+        .await
+        .unwrap();
+    play_sound(
         &music,
-        &mut processor,
-    ));
-    music.play_stream();
-    let mut pause: bool = false;
-    let mut p: i32 = 0;
-    let (mut samples, mut max_amp, mut m) = sample_handler.fft_wrapper();
-    while !rl.window_should_close() {
-        if rl.is_file_dropped() {
-            let dropped_files = rl.load_dropped_files();
-            let file_paths = dropped_files.paths();
-            if file_paths.len() > 0 {
-                let file_path = file_paths[0];
-                if file_path.contains(".mp3") || file_path.contains(".flac") {
-                    println!("New Music Dropped: {}", file_path);
-                    music.stop_stream();
-                    processor_handle = None;
-                    music = load_music(&audio, file_path.to_string());
-                    processor_handle = Some(attach_audio_stream_processor_to_music(
-                        &music,
-                        &mut processor,
-                    ));
-                    music.play_stream();
-                    pause = false;
-                }
-            }
-            for path in file_paths {
-                println!("Dropped path: {}", path);
-            }
+        PlaySoundParams {
+            looped: true,
+            volume: 1.,
+        },
+    );
+    let mut toggle: bool = false;
+    loop {
+        if is_key_down(KeyCode::Space) {
+            toggle = !toggle;
         }
-        music.update_stream();
-        if pause {
-            music.pause_stream();
+        if toggle {
+            audio::stop_sound(&music);
         } else {
-            music.resume_stream();
-        }
-        if rl.is_key_pressed(KeyboardKey::KEY_SPACE) {
-            pause = !pause;
-        }
-        let mut d = rl.begin_drawing(&thread);
-        d.clear_background(Color::BLACK);
-        if p % 25 == 0 {
-            (samples, max_amp, m) = sample_handler.fft_wrapper();
-        }
-        let sw = d.get_render_width() as f32;
-        let sh = d.get_render_height() as f32;
-        let cell_width = sw / m as f32;
-        let step = 1.059;
-        m = 0;
-        let mut f = 20.0;
-        while f < samples.len() as f32 {
-            let f1 = f * step;
-            let mut a = 0.0;
-            let mut q: usize = f as usize;
-
-            while q < samples.len() && q < (f1 as usize) {
-                a += samples[q];
-                q += 1;
-            }
-            a /= f1 - f + 1.0;
-            let t = a / max_amp;
-            d.draw_rectangle(
-                (m as f32 * cell_width) as i32,
-                ((sh) - 10.0 - (sh * t / 2.0)) as i32,
-                cell_width as i32,
-                (sh * t / 2.0) as i32,
-                Color::YELLOW,
+            play_sound(
+                &music,
+                PlaySoundParams {
+                    looped: true,
+                    volume: 1.,
+                },
             );
-            m += 1;
-            f *= step;
         }
-        p += 1;
+        clear_background(RED);
+        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
+        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
+        draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
+
+        draw_text("IT WORKS!", 20.0, 20.0, 30.0, DARKGRAY);
+
+        next_frame().await
     }
 }
