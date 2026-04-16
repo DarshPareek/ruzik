adf545e3a5eae9f02555b702bf891d9a978895f1
DarshPareek <pareekdarsh0162@gmail.com>
2026-04-16 19:13:13 +0530

    working visualizer

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,53 @@
 use raylib::prelude::*;
+
 mod fft;
 mod music;
 use music::*;
-const SAMPLE_SIZE: usize = 1 << 14;
+
+const SAMPLE_SIZE: usize = 1 << 12;
+
 fn main() {
     let (mut rl, thread) = raylib::init().size(1280, 720).title("Ruzik").build();
     let audio = RaylibAudio::init_audio_device().expect("Audio failed");
-    let music = load_music(
+
+    // 1. Make music mutable so we can reassign it later
+    let mut music = load_music(
         &audio,
-        "/home/darsh/devel/ruzik/assets/TestKick.mp3".to_string(),
+        "/home/darsh/devel/ruzik/assets/Let It Happen.flac".to_string(),
     );
     let sample_handler = init_sample_handler(SAMPLE_SIZE);
     let mut processor = processor(sample_handler.clone_samples());
-    let _processor_handle = attach_audio_stream_processor_to_music(&music, &mut processor);
+    let mut processor_handle = Some(attach_audio_stream_processor_to_music(
+        &music,
+        &mut processor,
+    ));
     music.play_stream();
     let mut pause: bool = false;
     let mut p: i32 = 0;
     let (mut samples, mut max_amp, mut m) = sample_handler.fft_wrapper();
     while !rl.window_should_close() {
+        if rl.is_file_dropped() {
+            let dropped_files = rl.load_dropped_files();
+            let file_paths = dropped_files.paths();
+            if file_paths.len() > 0 {
+                let file_path = file_paths[0];
+                if file_path.contains(".mp3") || file_path.contains(".flac") {
+                    println!("New Music Dropped: {}", file_path);
+                    music.stop_stream();
+                    processor_handle = None;
+                    music = load_music(&audio, file_path.to_string());
+                    processor_handle = Some(attach_audio_stream_processor_to_music(
+                        &music,
+                        &mut processor,
+                    ));
+                    music.play_stream();
+                    pause = false;
+                }
+            }
+            for path in file_paths {
+                println!("Dropped path: {}", path);
+            }
+        }
         music.update_stream();
         if pause {
             music.pause_stream();
@@ -29,10 +59,8 @@ fn main() {
         }
         let mut d = rl.begin_drawing(&thread);
         d.clear_background(Color::BLACK);
-        // // println!("***{:?}***", samples);
         if p % 25 == 0 {
             (samples, max_amp, m) = sample_handler.fft_wrapper();
-            println!("{:?}", samples.len());
         }
         let sw = d.get_render_width() as f32;
         let sh = d.get_render_height() as f32;
@@ -44,6 +72,7 @@ fn main() {
             let f1 = f * step;
             let mut a = 0.0;
             let mut q: usize = f as usize;
+
             while q < samples.len() && q < (f1 as usize) {
                 a += samples[q];
                 q += 1;
